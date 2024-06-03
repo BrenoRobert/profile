669e92f77262b486e3a1ced1b094d0840db39156
Breno Robert de Oliveira Ribeiro <59066325+BrenoRobert@users.noreply.github.com>
2024-06-03 10:36:44 -0300

    fix: update profile
    update of the latest news on the resume

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
-let autoTyped = new Typed("#auto-typing", {
+let autoTyped = new Typed('#auto-typing', {
   strings: [
-    `^1000 &nbsp; &nbsp; Olá. prazer em conhecê-lo.<br> Me chamo Breno Robert e espero que meu perfil possa atender a suas expectativas.
+    `^1000 &nbsp; &nbsp; Olá. prazer em conhecê-lo.<br> Me chamo Breno Robert e este é meu portfólio online.
  ^1000`,
   ],
   typeSpeed: 35,
   backSpeed: 25,
   loop: true,
-})
+});
 
 function getDescription(option) {
-  let description
+  let description;
 
   switch (option) {
-    case "profile-option":
+    case 'profile-option':
       description = `
         <p class="strong">Objetivo</p>
         <p>
           &nbsp; &nbsp; Desejo fazer parte do time da empresa oferecendo o
           melhor. Sou uma pessoa muito dedicada e comprometida com as
-          atribuições que são dadas, muito comunicativo e sempre me
-          esforçando para entregar o meu melhor.<br />
+          atribuições que são dadas.<br />
           &nbsp; &nbsp; Sou muito comunicativo e sempre me esforço para
-          entregar o meu melhor serviço, sendo sempre dedicado e pontual.
+          entregar o meu melhor.
         </p>
-        <p class="strong">Conhecimento técnico de tecnologias</p>
-        <p>Java, PHP, Laravel, JQuery, Python, Pandas, Power BI, ChatGPT, OpenAI.</p>
-        <p class="strong">Conhecimento sólidos de tecnologias</p>
+        <p class="strong">Conhecimento técnico e acadêmico</p>
+        <p>Java, PHP, Laravel, JQuery, Python, Pandas, Power BI, ChatGPT, OpenAI, Jira, Storybook, Jest, NextJS, Adobe Photoshop, Adobe Premier, SQL.</p>
+        <p class="strong">Conhecimento sólido</p>
         <p>
-          ReactJS, React Native, NestJS, Expo CLI, GitHub, Git, Web
-          Responsivo, HTML5, CSS3, Bootstrap, JavaScipt, TypeScript,
-          Redux, Context API, Figma, Adobe Photoshop.
+          ReactJS, React Native, NestJS, Expo, GitHub, Git, Web Responsivo, HTML5, CSS3, Bootstrap, JavaScipt, TypeScript, Redux, Context API, Draw.io, Figma, Styled-component, Vite, Tailwindcss, Shadcn UI.
         </p>
         <div>
           <span>Para me conhecer melhor,&nbsp;</span>
           <a
-            href="./assets/Curriculum Breno.pdf"
+            href="./assets/Curriculo-Breno.pdf"
             download
             class="cv-download"
             >baixe o currículo</a
           >
-        </div>`
-      break
-    case "education-option":
+        </div>`;
+      break;
+    case 'education-option':
       description = `
         <p class="strong">Formação</p>
         <p>
@@ -52,7 +49,7 @@ function getDescription(option) {
           - Curso Técnico _ SENAI - SP
           Agosto 2019 – Abril 2021
           <br>
-          &nbsp; &nbsp; Técnico Em Desenvolvimento De Sistemas
+          &nbsp; &nbsp; Desenvolvimento De Sistemas
           <br>
           <br>
           - Graduação _ Universidade Paulista - Unip
@@ -104,9 +101,15 @@ function getDescription(option) {
           <br>
           - React.Js e Next.Js (nível intermediário e avançado) _ Udemy – 2024
           <br>
-        </p>`
-      break
-    case "professional-option":
+          - NLW Expert trilha de Node.js – Rocketseat 2024
+          <br>
+          - NLW Unite trilha React.js – Rocketseat 2024
+          <br>
+          - SAP Primeiros Passos – Udemy 2024
+          <br>
+        </p>`;
+      break;
+    case 'professional-option':
       description = `
         <p class="strong">Experiências Profissionais</p>
         <p>
@@ -125,7 +128,7 @@ function getDescription(option) {
           <br>
           &nbsp; &nbsp; Março 2022 – Fevereiro 2023
           <br>
-          &nbsp; &nbsp; • Desenvolvimento de componentes, correção de bugs, teste e revisões de códigos em React Native, Expo, ReactJS e NestJS.
+          &nbsp; &nbsp; • Desenvolvimento de componentes, correção de bugs, teste e revisões de códigos em React Native, Expo, ReactJS e NestJS, outras tecnologias utilizadas como Styled-component, Storybook e Jest, Git, GitHub, Jira, Figma, Redux metodologia Kanban.
           <br>
           <br>
           - Águia Shoes
@@ -141,22 +144,24 @@ function getDescription(option) {
           <br>
           &nbsp; &nbsp; • Suporte de testes e verificação de rastreadores, desenho de diagrama de redes, suporte técnico na impressora.
         </p>
-        `
-      break
+        `;
+      break;
   }
 
-  return description
+  return description;
 }
 
 function typedProfile(el) {
-  let switchProfile = document.getElementById("switchProfile")
-  document.getElementById("auto-typing").parentElement.style.display = "none"
+  let switchProfile = document.getElementById('switchProfile');
+  document.getElementById('auto-typing').parentElement.style.display = 'none';
 
-  if (el.id === "profile-option") {
-    switchProfile.checked = true
-  } else if (el.id === "education-option") {
-    switchProfile.checked = false
+  if (el.id === 'profile-option') {
+    switchProfile.checked = true;
+  } else {
+    switchProfile.checked = false;
   }
 
-  document.querySelector("#panel-content div").innerHTML = getDescription(el.id)
+  document.querySelector('#panel-content div').innerHTML = getDescription(
+    el.id
+  );
 }
